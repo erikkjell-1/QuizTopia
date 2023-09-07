0b94e7c50947a21c165bd6441158b5f61dd4298d
erikkjell-1 <erkan26@live.se>
2023-09-07 10:24:50 +0200

    PUSH

--- quiz-project/src/components/game/Game.tsx
+++ quiz-project/src/components/game/Game.tsx
@@ -1,11 +1,87 @@
-function Game() {
-    return (
-        <div>
-            <h1>BLÅVITT ÅKER UR</h1>
-        </div>
-    )
+import { useState, useRef, useEffect } from 'react'
+import mapboxgl, { Map as MapGl } from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import './Game.css'
+import { ExistingQuiz } from '../Interface';
 
+async function getExistingQuiz() {
 
+    const [existingQuiz, setExistingQuiz] = useState<ExistingQuiz[]>([])
+
+    const url = 'https://fk7zu3f4gj.execute-api.eu-north-1.amazonaws.com/quiz'
+    const settings = {
+        method: 'GET',
+        headers:  {'Content-Type': 'application/json'}
+    }
+    const response = await fetch(url, settings)
+    const data: ExistingQuiz = await response.json()
+
+    if(data.existingQuiz){
+        setExistingQuiz(data.existingQuiz)
+    }
 }
 
-export default Game
+mapboxgl.accessToken = 'pk.eyJ1IjoiZXJpa2plbGwiLCJhIjoiY2xsdzJsZ24zMTN3NjNwbnowdnQ0eHEwMSJ9.B2PWYPIiQ9_-HwVynQloIA' as string
+
+
+function Game() {
+    const mapContainer = useRef(null)
+    const mapRef = useRef<MapGl | null>(null)
+    const [lat, setLat] = useState<number>(58.6551)
+    const [lng, setLng] = useState<number>(17.0583)
+    const [zoom, setZoom] = useState<number>(12)
+
+    const [quizTitle, setQuizTitle] = useState<string>('')
+    const [quizQ, setQuizQ] = useState<string>('')
+    const [quizA, setQuizA] = useState<string>('')
+
+    
+    useEffect(() => {
+        if( mapRef.current || !mapContainer.current ) return
+
+        mapRef.current = new MapGl({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v12',
+            center: [lng, lat],
+            zoom: zoom
+        });
+
+    const map: MapGl = mapRef.current
+    
+    map.on('move', () => {
+        interface Position {
+            lng: number;
+            lat: number;
+        }
+        const position: Position = map.getCenter()
+        setLat(Number(position.lat.toFixed(4)))
+        setLng(Number(position.lng.toFixed(4)))
+        setZoom(map.getZoom());
+    })
+    },[lat, lng, zoom]
+
+    return (
+
+        <div>
+            <div className='quiz__container'>
+                <input className='quiz--input' placeholder='Quiz-Namn' type='text' value={quizTitle} onChange={event => setQuizTitle(event.target.value)}></input>
+                <input className='quiz--input' placeholder='Quiz-Fråga?' type='text' value={quizQ} onChange={event => setQuizQ(event.target.value)}></input>
+                <input className='quiz--input' placeholder='Quiz-Svar' type='text' value={quizA} onChange={event => setQuizA(event.target.value)}></input>
+
+                <button onClick={addQuiz}>Skapa Quiz</button>
+
+            </div>
+
+            <div className='map__map'>
+            <div ref={mapContainer} className="map-container" />
+			<p> Center position: {lat} lat, {lng} lng </p>
+            </div>
+
+            <div className='getQuiz__container'>
+                <button onClick={getExistingQuiz}>Show existing quizes</button>
+            </div>
+        </div>
+    
+    )}
+   
+    export default Game
